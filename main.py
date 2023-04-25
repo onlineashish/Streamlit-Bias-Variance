9067171c58e5a392b75b165e06fef14ac0ce0211
onlineashish <me.ashish2525.as@gmail.com>
2023-04-25 23:35:11 +0530

    added error display

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ np.random.seed(42)
 # x = np.random.rand(100) * 20
 # y = 50 * x + x**2 + np.random.randn(100) * 100
 
-x = np.arange(1,51)
-y = x**2+3
-# x = [1,2,3,4,5,6,7,8,9,10]
-# y = [1,4,9,16,25,36,49,64,81,100]
+# x = np.arange(1,51)
+# y = x**2+3
+x = [1,2,3,4,5,6,7,8,9,10]
+y = [1,4,9,16,25,36,49,64,81,100]
 
 # Create DataFrame
 df = pd.DataFrame({'x': x, 'y': y})
@@ -23,7 +23,7 @@ df = pd.DataFrame({'x': x, 'y': y})
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(df[['x']], df['y'], test_size=0.2 ,random_state=42)
 
-k= st.sidebar.slider("K Value", min_value=1,max_value=len(X_train)//2,step=2)
+k= st.sidebar.slider("K Value", min_value=1,max_value=len(X_train)//2,step=1)
 
 knn = KNeighborsRegressor(n_neighbors=k,weights='distance')
 knn.fit(X_train, y_train)
@@ -39,19 +39,10 @@ def addlabels(x,y):
         plt.text(i, y[i], y[i], ha = 'center')
 
 
-#streamlit plot for bias and variance on varying k
-nam = ['bias', 'variance']
-val = [bias, variance]
-plt.bar(nam, val )
 
-plt.title('Bias and Variance')
-plt.xlabel('Bias and Variance')
-plt.ylabel('Value')
-addlabels(nam, val)
-
-plt.show()
-fig = plt.gcf()
-st.pyplot(fig)
+st.write("Mean Squared Error: ", error)
+st.write("Bias: ", bias)
+st.write("Variance: ", variance)
 
 # Plot the data
 plt.scatter(X_train, y_train, label='Trained values')
@@ -67,4 +58,19 @@ plt.show()
 fig = plt.gcf()
 st.pyplot(fig)
 
+
+
+#streamlit plot for bias and variance on varying k
+nam = ['bias', 'variance']
+val = [bias, variance]
+plt.bar(nam, val )
+
+plt.title('Bias and Variance')
+plt.xlabel('Bias and Variance')
+plt.ylabel('Value')
+addlabels(nam, val)
+
+plt.show()
+fig = plt.gcf()
+st.pyplot(fig)
 
